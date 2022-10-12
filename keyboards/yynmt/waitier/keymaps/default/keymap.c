5547472d2746979a8ca5f98b7c7973d5479a41e8
yynmt <yynmt@yynmt.com>
2022-10-13 03:43:02 +0900

    first commit

---
+++ keyboards/yynmt/waitier/keymaps/default/keymap.c
@@ -0,0 +1,29 @@
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {USER00 = SAFE_RANGE};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_ortho_1x1(USER00)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case USER00:
+      if (record->event.pressed) {
+        switch (rand() % 3) {
+          case 0:
+            send_string("If you accept the pain, I will forgive you.");
+            break;
+          case 1:
+            send_string("If you accept my pain, can forgive you.");
+            break;
+          case 2:
+            send_string("Even if my(yours) vision becomes glazed, our suffering is ours alone.");
+            break;
+        }
+      }
+      return false;
+      break;
+  }
+  return true;
+}
---
+++ keyboards/yynmt/waitier/keymaps/via/keymap.c
@@ -0,0 +1,27 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_ortho_1x1(USER00)
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case USER00:
+    if (record->event.pressed) {
+      switch (rand() % 3) {
+        case 0:
+          send_string("If you accept the pain, I will forgive you.");
+          break;
+        case 1:
+          send_string("If you accept my pain, can forgive you.");
+          break;
+        case 2:
+          send_string("Even if my(yours) vision becomes glazed, our suffering is ours alone.");
+          break;
+      }
+      }
+      return false;
+      break;
+  }
+  return true;
+}
