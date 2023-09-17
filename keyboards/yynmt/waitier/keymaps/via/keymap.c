144777ad23c5ab5d328cee329534365f31bc5617
yynmt <yynmt@yynmt.com>
2023-09-17 19:20:35 +0900

    Support for waitier to 0.22.3

--- keyboards/yynmt/waitier/keymaps/via/keymap.c
+++ keyboards/yynmt/waitier/keymaps/via/keymap.c
@@ -1,12 +1,12 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ortho_1x1(USER00)
+  [0] = LAYOUT_ortho_1x1(QK_KB_0)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case USER00:
+    case QK_KB_0:
     if (record->event.pressed) {
       switch (rand() % 3) {
         case 0:
